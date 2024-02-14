432956e3d5b0023af71aeec192675200503860d5
Adil <adil.souirite@gmail.com>
2024-02-14 13:38:08 +0000

    create tracker for fault.

---
+++ plcmon/plcmon/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace plcmon {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tipadress = new System.Windows.Forms.TextBox();
+            this.btnconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "PLC IP Adress : ";
+            // 
+            // tipadress
+            // 
+            this.tipadress.Location = new System.Drawing.Point(26, 27);
+            this.tipadress.Name = "tipadress";
+            this.tipadress.Size = new System.Drawing.Size(172, 20);
+            this.tipadress.TabIndex = 15;
+            // 
+            // btnconnect
+            // 
+            this.btnconnect.Location = new System.Drawing.Point(204, 24);
+            this.btnconnect.Name = "btnconnect";
+            this.btnconnect.Size = new System.Drawing.Size(120, 24);
+            this.btnconnect.TabIndex = 14;
+            this.btnconnect.Text = "Connect to PLC";
+            this.btnconnect.UseVisualStyleBackColor = true;
+            this.btnconnect.Click += new System.EventHandler(this.btnconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tipadress);
+            this.Controls.Add(this.btnconnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tipadress;
+        private System.Windows.Forms.Button btnconnect;
+    }
+}
+
---
+++ plcmon/plcmon/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Sharp7;
+using static System.Windows.Forms.LinkLabel;
+
+namespace plcmon {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public S7Client client = new S7Client();
+        public bool connected =false;
+        public System.Timers.Timer timer;
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            ShowFaults();
+        }
+        private void btnconnect_Click(object sender, EventArgs e)
+        {
+            string ipaddress = tipadress.Text;
+
+            if(ipaddress != "")
+            {
+                if(!connected)
+                {
+                    int connectionResult = client.ConnectTo(ipaddress, 0, 1);
+                    if (connectionResult == 0)
+                    {
+                        btnconnect.BackColor = Color.LightGreen;
+                        btnconnect.Text = "Disconnect from PLC";
+                        connected = true;
+                        if (timer == null)
+                        {
+                            timer = new System.Timers.Timer();
+                            timer.Elapsed += Timer_Elapsed;
+                            timer.Start();
+                        }
+
+                    }
+                } else
+                {
+                    lient.Disconnect();
+                    btnconnect.BackColor = Color.Red;
+                    btnconnect.Text = "Connect to PLC";
+                    timer.Stop();
+                    connected = false;
+                }
+            }
+        }
+        public void ShowFaults()
+        {
+            const int area = S7Consts.S7AreaDB; // DB area
+            const int dbNumber = 1020; // DB number
+            const int start = 30; // Byte offset of the integer within the DB
+            const int amount = 30; // Number of bytes to read (2 bytes for an integer)
+            Dictionary<string, object> tracker = new Dictionary<string, object>();
+
+            byte[] dbBuffer = new byte[30];
+            int result = client.DBRead(dbNumber, start, amount, dbBuffer);
+
+            for (int i = 0; i < 30; i += 2)
+            {
+                // Convert the bytes to an integer
+                int value = S7.GetIntAt(dbBuffer, i); // Adjust byte index if needed
+                var bools = new BitArray(new int[] { value }).Cast<bool>().ToArray();
+                for (int j = 0; j < bools.Length; j++)
+                {
+                    string key = $"DB1020.DBW{i + 30}.DBX{j}";
+                    if (bools[j])
+                    {
+                        if (!tracker.ContainsKey(key))
+                        {
+                            tracker[key] = new List<string> { DateTime.Now.ToString() };
+                        }
+                    } else
+                    {
+                        if (tracker.ContainsKey(key))
+                        {
+                            tracker[key].Add(DateTime.Now.ToString());
+                        }
+                    }
+                    
+                }
+
+            }
+
+        }
+    }
+}
